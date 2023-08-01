4c1004416b7baa4b92ccf68c7daa558a082c4436
SaifulJnU <saiful2561998@gmail.com>
2023-08-01 12:47:11 +0600

    added middleware

--- main.go
+++ main.go
@@ -7,8 +7,29 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/saifuljnu/crudApp/database"
 	"github.com/saifuljnu/crudApp/handler"
+	"github.com/saifuljnu/crudApp/middlewares"
 	"github.com/saifuljnu/crudApp/service"
 )
+
+// LoggerMiddleware logs the client IP, Response HTTP Status Code, and Latency for every request
+// func LoggerMiddleware() gin.HandlerFunc {
+// 	return func(c *gin.Context) {
+// 		startTime := time.Now()
+
+// 		// for taking client ip
+// 		clientIP := c.ClientIP()
+
+// 		// for calling the next handler in the chain
+// 		c.Next()
+
+// 		// for latency
+// 		latency := time.Since(startTime)
+
+// 		// Log the info
+// 		log.Printf("Client IP: %s | Status Code: %d | Latency: %s | RequestMethod: %s | RequestURI: %s",
+// 			clientIP, c.Writer.Status(), latency, c.Request.Method, c.Request.RequestURI)
+// 	}
+// }
 
 func main() {
 	// Set Gin to release mode.
@@ -26,17 +47,20 @@ func main() {
 	// Create a new Gin router.
 	r := gin.Default()
 
+	// Using the custom middleware for every route
+	//r.Use(LoggerMiddleware())
+
+	r.Use(gin.Recovery(), middlewares.Logger())
+
 	// Create instances of services and handlers
 	userService := service.NewUserService(dbClient)
 	userHandler := handler.NewUserHandler(userService)
 
-	// Define API routes and handlers
 	r.POST("/api/collectionUser", userHandler.InsertUser)
 	r.GET("/api/collectionUser", userHandler.GetAllUsers)
 	r.DELETE("/api/collectionUser/:id", userHandler.DeleteUser)
 	r.PUT("/api/collectionUser/:id", userHandler.UpdateUser)
 
-	// Start the server
 	fmt.Println("Server started on http://localhost:3000")
 	r.Run(":3000")
 }
---
+++ middlewares/logger.go
@@ -0,0 +1,23 @@
+package middlewares
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Logger() gin.HandlerFunc {
+	return gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+		return fmt.Sprintf("Client IP: %s\nTime: [%s]\nRequestMethod: %s\nRequestURI: %s\nStatusCode: %d\nLatency: %s\n",
+			param.ClientIP,
+			param.TimeStamp.Format(time.RFC822),
+			param.Method,
+			param.Path,
+			param.StatusCode,
+			param.Latency,
+		)
+
+	})
+
+}
